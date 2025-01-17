3973cf7db299c89a7c8d950dcc121bb7426bd66c
jahan siddiqui <raj918833@gmail.com>
2025-01-18 03:12:50 +0530

    mongodb connected and schema created for user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
+dotenv.config();
 const app = express();
-const PORT = 5001;
 
-app.use("/api/auth", authRoutes)
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on Port ${PORT}`);
+  connectDB();
 });
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      uniqu: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.model("User", userSchema);
