5f77a70e3609bdc3421c1a0c2072a77e72d276d5
jahan siddiqui <raj918833@gmail.com>
2025-01-20 02:54:42 +0530

    logout auth route api done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -80,5 +80,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.send("logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Error in logout controller:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
 };
