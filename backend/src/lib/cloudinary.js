56a27531375d05ebab5c4b36a56c2476f005c1e0
jahan siddiqui <raj918833@gmail.com>
2025-01-20 21:11:03 +0530

    cloudinary configuration done for future image upload

---
+++ backend/src/lib/cloudinary.js
@@ -0,0 +1,15 @@
+
+import { v2 as cloudinary } from "cloudinary";
+import { config } from "dotenv";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+if (!process.env.CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+    throw new Error("Missing Cloudinary configuration in .env file");
+  }
+
+export default cloudinary;
