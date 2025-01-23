2c6137d656504993dc440a33bba7a4bfae395251
jahan siddiqui <raj918833@gmail.com>
2025-01-24 03:48:22 +0530

    components and pages skeleton done for future codes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+
 function App() {
   return (
-    <>
-      <div className=" font-poppins">
-        <h1 className="text-7xl text-copy  font-semibold tracking-tight">Hello world</h1>
-        <button className="btn btn-neutral">Neutral</button>
-<button className="btn bg-secondary">Primary</button>
-
-      </div>
-    </>
+    <div className=" font-poppins">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </div>
   );
 }
 
